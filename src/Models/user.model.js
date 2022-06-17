a3a0d6519be5a0435e1d22403fc0fc60133900ef
RohanRedekar <rohanredekar100@gmail.com>
2022-06-17 15:29:27 +0530

    user post and get method done

---
+++ src/Models/user.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    phone: {type: Number, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+},{
+    timestamps: true,
+    versionKey: false
+})
+
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
--- src/index.js
+++ src/index.js
@@ -4,4 +4,9 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
+
+const userController = require("./Controllers/user.controller");
+
+app.use("/users", userController);
+
 module.exports = app;
